03c859fdf17fd2fd5d9c419ffc8d47364a402bbd
rajmi <rajminakarmi26@gmail.com>
2020-08-01 19:02:03 -0400

    add in login.java

---
+++ src/login.java
@@ -0,0 +1,12 @@
+public class login {
+    public static void main(String[] args) {
+        int a =1;
+        int b= 2;
+          if(a==b){
+              System.out.println("you are right");
+
+          }else{
+              System.out.println("think one more time");
+          }
+    }
+}
