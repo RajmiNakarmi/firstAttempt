a4ff0980dc29ea2fb6bef46bd8a82b02cfdea5ef
rajmi <rajminakarmi26@gmail.com>
2020-08-01 16:12:56 -0400

    add namaste.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,5 +2,6 @@ public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello again");
+        System.out.println("how are you?");
     }
 }
