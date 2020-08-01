5ef441f8d0ccf728fd6faf5ad0c6d63f27715d02
RajmiNakarmi <68827515+RajmiNakarmi@users.noreply.github.com>
2020-08-01 18:17:35 -0400

    Update HelloB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -2,7 +2,7 @@ public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello again");
-        System.out.println("how are you?");
+        System.out.println("how are you today?");
         System.out.println("i am well.");
     }
 }
